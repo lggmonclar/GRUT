b80684ca5ce81f4a6cc642f108ef45f80e691dcf
Luiz Monclar <lggmonclar@gmail.com>
2019-03-16 12:24:05 -0300

    Clean up Line class implementation to not use unnecessary scoping

--- GRUT Engine/src/Core/Debugging/Line.cpp
+++ GRUT Engine/src/Core/Debugging/Line.cpp
@@ -9,17 +9,17 @@ namespace GRUT {
   unsigned int Line::s_VBO = 0;
 
   void Line::Initialize() {
-    GRUT::Line::s_lineShader.LoadVertexShader("../../GRUT Engine/GRUT Engine/prefabs/shaders/line.vert");
-    GRUT::Line::s_lineShader.LoadFragmentShader("../../GRUT Engine/GRUT Engine/prefabs/shaders/line.frag");
+    s_lineShader.LoadVertexShader("../../GRUT Engine/GRUT Engine/prefabs/shaders/line.vert");
+    s_lineShader.LoadFragmentShader("../../GRUT Engine/GRUT Engine/prefabs/shaders/line.frag");
     RenderManager::Instance().RegisterRenderCallback([=] {
-      glGenBuffers(1, &GRUT::Line::s_VBO);
-      glGenVertexArrays(1, &GRUT::Line::s_VAO);
+      glGenBuffers(1, &s_VBO);
+      glGenVertexArrays(1, &s_VAO);
     }, CallbackTime::PRE_RENDER, true);
   }
   void Line::Draw(Vector<3> p_startPos, Vector<3> p_endPos, Vector<3> p_color) {
     if (!s_hasInitialized) {
-      s_hasInitialized = true;
       Initialize();
+      s_hasInitialized = true;
     }
 
     float vertices[] = {
@@ -28,13 +28,13 @@ namespace GRUT {
     };
 
     RenderManager::Instance().RegisterSingleFrameRenderCallback([=] {
-      if (!GRUT::Line::s_VAO || !GRUT::Line::s_VBO) return;
+      if (!s_VAO || !s_VBO) return;
 
-      GRUT::Line::s_lineShader.Use();
-      GRUT::Line::s_lineShader.SetVec3("lineColor", p_color);
+      s_lineShader.Use();
+      s_lineShader.SetVec3("lineColor", p_color);
 
-      glBindVertexArray(GRUT::Line::s_VAO);
-      glBindBuffer(GL_ARRAY_BUFFER, GRUT::Line::s_VBO);
+      glBindVertexArray(s_VAO);
+      glBindBuffer(GL_ARRAY_BUFFER, s_VBO);
       glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
       glEnableVertexAttribArray(0);
